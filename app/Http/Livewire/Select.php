77f9e143e55bc5ff644b2de7765d52cc4348b283
thivalls <olavalls@gmail.com>
2021-01-11 15:32:00 -0300

    Ending up dp

--- app/Http/Livewire/Select.php
+++ app/Http/Livewire/Select.php
@@ -5,6 +5,24 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 class Select extends Component
 {
+    public $states;
+    public $cities;
+    public $selectedState;
+    public $selectedCity;
+    public $showCities = false;
+
+    public function mount() {
+        $response = \Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome");
+        $this->states = json_decode($response->body(), true);
+    }
+
+    // o $value vai ser a sigla do estado
+    public function updatedSelectedState($value) {
+        $this->showCities = true;
+        $response = \Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/{$value}/municipios?orderBy=nome");
+        $this->cities = json_decode($response->body(), true);
+    }
+
     public function render()
     {
         return view('livewire.select');
--- resources/views/livewire/select.blade.php
+++ resources/views/livewire/select.blade.php
@@ -1,14 +1,16 @@
-<div>
-    <div class="w-full h-screen max-w-2xl mx-auto flex flex-col justify-center items-center">
+<div wire:loading.class.delay="opacity-30">
+    <div class="w-full h-screen max-w-2xl mx-auto flex flex-col mt-20">
         <h1 class="text-5xl antialiased mb-8">Dropdown dependente</h1>
 
         <div class="flex w-full">
             <div class="w-1/2">
-                <x-select-state label="Estado"/>
+                <x-select-state wire:model="selectedState" :states="$states" label="Estado"/>
             </div>
-            <div class="w-1/2">
-                <x-select-city label="Cidade"/>
-            </div>
+            @if($showCities)
+                <div class="w-1/2">
+                    <x-select-city wire:model="selectedCity" :cities="$cities" label="Cidade"/>
+                </div>
+            @endif
         </div>
     </div>
 </div>
