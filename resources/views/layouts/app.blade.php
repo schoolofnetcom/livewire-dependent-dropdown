f8df707f248ec4e148eb000e580b067d7f01f88e
thivalls <olavalls@gmail.com>
2021-01-11 11:44:52 -0300

    Instaling tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,12 @@
 <html>
     <head>
+        <link rel="stylesheet" href="{{ asset('app.css') }}">
         @livewireStyles
     </head>
     <body>
         @yield('content')
+
+        <script src="{{ asset('app.js') }}"></script>
         @livewireScripts
     </body>
 </html>
